efaa0a1dd067b8e6125890c755d14490f86aa656
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-08-26 01:24:39 -0500

    3

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {RiHome6Line, RiPercentLine, RiPieChartLine, RiMailLine, RiNotification3Line, RiSettings4Line} from 'react-icons/ri'
+import {RiHome6Line, RiPercentLine, RiPieChartLine, RiMailLine, RiNotification3Line, RiSettings4Line, RiLogoutCircleLine} from 'react-icons/ri'
 const Sidebar = () => {
     return (
-        <div className='bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full'>
+        <div className='bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl'>
             <div>
                 <ul className='pl-4'>
                     <h1 className='text-2xl text-gray-300 uppercase font-bold text-center my-5'>
@@ -40,7 +40,15 @@ const Sidebar = () => {
                     </li>
                 </ul>
             </div>
-            
+            <div>
+                <ul className='pl-4'>
+                <li className='hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors'>
+                        <a href='#' className='group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white transition-colors'>
+                            <RiLogoutCircleLine className='text-2xl' />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
